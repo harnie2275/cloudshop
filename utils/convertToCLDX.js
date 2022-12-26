2a8a412840839ef42d47718efed2315e4efaf173
harnie2275 <orizon24mail@gmail.com>
2022-12-26 20:04:45 +0100

    omooooo

--- utils/convertToCLDX.js
+++ utils/convertToCLDX.js
@@ -1,3 +1,16 @@
-exports.convertToCLDX = (amount) => {
-  return parseFloat(amount / 100) / 0.001;
+const { default: axios } = require("axios");
+
+exports.convertToCLDX = async (amount) => {
+  var config2 = {
+    method: "get",
+    url: `https://api.flutterwave.com/v3/rates?from=NGN&to=USD&amount=${amount}`,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer FLWSECK-c67abb2869dab7a8ae3f95c69ad5e90e-X",
+    },
+  };
+
+  let rate = await axios(config2);
+  const usdConverted = rate.data.data.rate;
+  return parseFloat(usdConverted / 100) / 0.001;
 };
