df66825f620e530b0194c802d596089e1056f018
winston <sheanwinston@gmail.com>
2023-04-03 10:06:23 +0100

    upload data fix

--- routes/verification.routes.js
+++ routes/verification.routes.js
@@ -1,13 +1,13 @@
 const express = require("express");
+const { uploadData } = require("../controllers/Vendor/Verification");
 const router = express.Router();
 const { vendorMiddleware } = require("../middleware/vendorMiddleware");
-
 
 router.post("/callback", function (req, res, next) {
     console.log(req.body);
 });
 
-router.post("/upload-data", );
+router.post("/upload-data", uploadData );
 
 
 module.exports = router;
